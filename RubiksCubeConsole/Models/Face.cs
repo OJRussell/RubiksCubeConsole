1b2e97bdff9d52556e3555aef5f7e8fa847d3ae2
OJRussell <60736274+OJRussell@users.noreply.github.com>
2022-08-20 12:59:32 +0100

    Add files via upload

---
+++ RubiksCubeConsole/Models/Face.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using RubiksCubeConsole.Enums;
+
+namespace RubiksCubeConsole.Models
+{
+    public class Face
+    {
+        public List<Cube> Cubes { get; set; } = new List<Cube>();
+
+        public Face(Colours colour)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Cubes.Add(new Cube(colour));
+            }
+        }
+    }
+}
+
